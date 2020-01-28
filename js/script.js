91a107e00d565b33bdf096eff5c0841c1c1d3b52
Remlix <jachan12@s.sfusd.edu>
2020-01-28 02:27:08 +0000

    song and artists

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-let songNames = ["thebox" , "Paramedic" , "brb" , "lifestyle"]
+let songNames = ["thebox" , "Paramedic" , "Brb" , "idfc"]
+let artists = ["roddy" , "SOBxRBE" , "LuhKel" , "blackbear"]
 songNames.forEach(function(song){
     console.log(song + " ") ;
-$(".songList").append("<p>" + song + "</p>")
+    $(".songList").append("<p>" + song + "</p>")
+
+
 });
+artists.forEach(function(creator){
+    $(".artistList").append("<p>" + creator + "</p>")
+});
+var input = $("inbox").val();
